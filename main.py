8a29d09ce3556c1d0321e670288742eca0179f9a
Dheerajmali <114793967+Dheerajmali@users.noreply.github.com>
2022-10-12 22:03:42 +0530

    python

---
+++ main.py
@@ -0,0 +1,28 @@
+#x = 'Hi everyone'
+#print(x.upper())
+#print(x.lower()) 
+
+# #remove whitespaces
+
+#x =  "   Lets study python"
+#print(x.strip())
+
+#replace
+
+#x = 'Hello world'
+#print(x.replace('H','A'))
+
+#splitting a string
+
+#x = 'hello world'
+#print(x.split())
+
+#x = 'hello world'
+#y = x.split()
+#print(y[0])
+# String concatenation
+
+#x = "hello"
+#y = "world"
+#z = x + " "+ y
+#print(z)
